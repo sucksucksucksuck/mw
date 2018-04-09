239136d06c94676c31ff2d75c7810e4b1e2e3fa7
linqiuchan <328611950@qq.com>
2018-04-09 14:50:54 +0800

    login

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,17 @@
   <div id="app">
     <header>
       <img src="./assets/images/logo.png" alt="">
+      <div class="btn">
+        <button type="button">About Us</button>
+        <button type="button">Help</button>
+      </div>
     </header>
-    <router-view/>
+    <div class="wrap">
+        <router-view/>
+    </div>
+    <footer>
+      hi
+    </footer>
   </div>
 </template>
 
@@ -24,11 +33,55 @@ export default {
   height: 100%;
   display: flex;
   flex-direction: column;
+  flex: 1;
+  width: 100%;
+  overflow: hidden;
+  position: absolute;
+  top: 0;
+  left: 0;
+}
+header {
+  background: #282E2C;
+  height: 15%;
+  justify-content: space-between;
+  align-items: center;
+  display: flex;
+}
+header .btn {
+  margin-right: 30px;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-self: flex-end;
+}
+header .btn button {
+  margin-left: 15px;
+  border: none;
+  background: rgba(225, 255, 255, 0.2);
+  color:#fff;
+  border-top-left-radius: 17px;
+  border-top-right-radius: 17px;
+  padding: 8px 20px 4px 20px;
+  cursor: pointer;
+  outline: none;
+  font-weight: 600;
+}
+
+.wrap {
+  flex:1;
+  display: flex;
+  background: #fff;
 }
 header img {
-  width: 10%;
+  height: 40%;
+  margin-left: 30px;
 }
 * {
   margin: 0;
+}
+footer {
+  background: #282E2C;
+  height: 15%;
+  justify-content: center;
 }
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,13 +1,21 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>zhuc</h2>
-    <button>
-      傻逼
-    </button>
-    <button @click='sb'>
-      二百五
-    </button>
+  <div class="login">
+    <div class="panel">
+        <div class="item">
+          账号：<input type="text" name="account">
+        </div>
+        <div class="item">
+            密码：<input type="text" name="password">
+          </div>
+          <div class="btns">
+            <button type="button">
+              登录
+            </button>
+            <button type="button">
+              注册
+            </button>
+          </div>
+    </div>
   </div>
 </template>
 
@@ -16,7 +24,7 @@ export default {
   name: 'HelloWorld',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App2升水'
+      msg: 'Welcome'
     }
   },
   methods: {
@@ -29,6 +37,54 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+  .login {
+    display: flex;
+    flex:1;
+    background: url(../assets/images/bg.png) no-repeat;
+    background-size:100% 100%;
+    justify-content: center;
+    align-items: flex-end;
+    flex-direction: column;
+  }
+  .login .panel {
+    background:rgba(255, 255, 255, 0.2);
+    height: 30%;
+    margin-right:10%;
+    border-radius: 20px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    font-size: 14px;
+  }
+  .login .panel .item {
+    padding: 0 10px;
+    color: #000;
+    padding-top:10px;
+  }
+  .login .panel .item input {
+    width: 120px;
+    border-radius: 10px;
+    border: none;
+    height: 20px;
+    line-height: 20px;
+    outline: none;
+    padding-left:10px;
+  }
+  .login .panel .btns {
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+  }
+  .login .panel .btns button {
+  margin-left: 15px;
+  border: none;
+  background: rgba(61, 61, 61, 0.2);
+  color:#000;
+  border-radius: 12px;
+  padding:5px 15px;
+  cursor: pointer;
+  outline: none;
+}
 h1, h2 {
   font-weight: normal;
 }
